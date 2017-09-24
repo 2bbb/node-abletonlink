679eafa47b6216c2fecf418508c8087e1be170b1
ISHII 2bit <ishii@buffer-renaiss.com>
2017-09-24 20:46:10 +0900

    tiny update

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const binding = nbind.init(__dirname);
 const lib = binding.lib;
 
 lib.AbletonLink.prototype.startUpdate = function(interval_ms, callback) {
+    this.update();
     if(callback) {
         this.timer = setInterval(() => {
             this.update();
             callback(this.beat, this.phase, this.bpm);
         }, interval_ms);
+        callback(this.beat, this.phase, this.bpm);
     } else {
         this.timer = setInterval(() => {
             this.update();
         }, interval_ms);
     }
-    this.update();
 };
 
 lib.AbletonLink.prototype.stopUpdate = function() {
