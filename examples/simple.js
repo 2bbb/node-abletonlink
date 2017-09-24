f3c215c915019126b447e138619c8eebaa1a12e7
ISHII 2bit <ishii@buffer-renaiss.com>
2017-09-24 20:46:21 +0900

    update example

--- examples/simple.js
+++ examples/simple.js
@@ -1,11 +1,11 @@
 const abletonlink = require('../index.js');
 
 // const link = new abletonlink(bpm = 120.0, quantum = 4.0, enable = true);
-const link = new abletonlink(160.0);
+const link = new abletonlink(160.0, 3.0, false);
 
 link.on('tempo', (tempo) => console.log("tempo", tempo));
 link.on('numPeers', (numPeers) => console.log("numPeers", numPeers));
-link.startUpdate(60, (beat, phase, bpm) => console.log("updated", beat, phase, bpm));
+link.startUpdate(16, (beat, phase, bpm) => console.log("updated", beat, phase, bpm));
 // or link.startUpdate(60);
 
 setInterval(() => {
