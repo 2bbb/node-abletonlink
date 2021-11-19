158e318abbe9c34f4ffdc7129f1c8e5e267bbdd9
2bit <github@2bit.jp>
2021-11-19 14:21:00 +0900

    fixed index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,7 @@
-declare module 'abletonlink' {
-  export const Audio = class AbletonLinkAudio extends AbletonLinkBase {}
-  export = class AbletonLink extends AbletonLinkBase {}
-}
+type EventTypes = 'tempo' | 'numPeers' | 'playState';
+type updateCallback = (beat:number, phase: number, bpm: number, playState: boolean) => any;
 
-type eventTypes = 'tempo' | 'numPeers' | 'playState'
-type updateCallback = (beat:number, phase: number, bpm: number, playState: boolean) => any
-
-abstract class AbletonLinkBase {
-  // native code
-  constructor(bpm?: number, quantum?: number, enable?: boolean)
+declare class AbletonLinkBase {
   setBeatForce(beat: number): void
   play(): void
   stop(): void
@@ -21,9 +14,26 @@ abstract class AbletonLinkBase {
   onTempoChanged(cb: Function): void
   onNumPeersChanged(cb: Function): void
   onPlayStateChanged(cb: Function): void
-  on(key:eventTypes, cb: Function): void
-  off(key:eventTypes): void
+  on(key: 'tempo', cb: (tempo: number) => any): void
+  on(key: 'numPeers', cb: (num_peers: number) => any): void
+  on(key: 'playState', cb: (play_state: boolean) => any): void
+  on(key: EventTypes, cb: Function): void
+  off(key: EventTypes, cb?: Function): void
   // JavaScript
+  startUpdate(quantum: number, cb: updateCallback): void
   startUpdate(interval_ms: number, cb?: updateCallback): void
   stopUpdate(): void
+};
+
+export default class AbletonLink extends AbletonLinkBase {
+  constructor(bpm?: number, quantum?: number, enable?: boolean): AbletonLink;
+};
+
+class AbletonLinkAudio extends AbletonLinkBase {
+  constructor(bpm?: number, quantum?: number, enable?: boolean): AbletonLinkAudio;
+};
+
+declare namespace AbletonLink {
+    export const Audio: typeof AbletonLinkAudio;
 }
+
